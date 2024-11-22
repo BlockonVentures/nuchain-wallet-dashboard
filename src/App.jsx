b8598dc9af6cd5a3ba1e415466980535906478b2
Alok <alok965801@gmail.com>
2024-11-22 19:04:19 +0530

    files and folder structure is ready and css files coomon properties added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import LayOut from './utils/layout/LayOut';
+
+
+function App() {
+
+  return (
+    <>
+     <LayOut/>
+    </>
+  )
+}
+
+export default App
