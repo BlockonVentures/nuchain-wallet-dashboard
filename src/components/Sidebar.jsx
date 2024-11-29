67fdd957f1971b23d274bc9e2e26a2f16181c233
Shalini <chauhanshaliniuk@gmail.com>
2024-11-29 16:23:31 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,70 @@
 import React from "react";
 import Logo from "../assets/images/Logo.png";
+import home from "../assets/images/home.png";
+import liquidityPool from "../assets/images/liquidity-pool.png";
+import paymentGateway from "../assets/images/payment-gateway.png";
+import profile from "../assets/images/profile.png";
+import stakingPool from "../assets/images/staking-pool.png";
+import transactionManagement from "../assets/images/transaction-management.png";
+import userManagement from "../assets/images/user-management.png";
 
 const Sidebar = () => {
   return (
-    <div className="hidden sm:block w-1/5 bg-white shadow-md">
-      <div className="p-4 text-purple-700 font-bold text-xl">
-        <img src={Logo} alt="logo" />
+    <div className="hidden sm:block w-[26%] bg-white shadow-lg h-screen">
+      {/* Logo Section */}
+      <div className="p-4 flex items-center mb-7">
+        <img src={Logo} alt="logo" className="h-12 w-auto" />
       </div>
-      <ul className="space-y-2 p-4">
-        <li className="text-gray-700 hover:bg-purple-100 p-2 rounded">
-          Dashboard
+
+      {/* Navigation Menu */}
+      <ul className="space-y-6 pl-0 px-4">
+        <li className="text-[#6F7074] text-[11.25px] flex items-center rounded-lg cursor-pointer transition-all duration-200">
+          {/* <span className="material-icons-outlined mr-3 text-purple-700">
+            dashboard
+          </span> */}
+          <span className="w-5 h-5">
+            <img src={home} alt="home" />
+          </span>
+
+          <span className="ml-1 text-[#8712C2] text-xs font-bold">
+            Dashboard
+          </span>
         </li>
-        <li className="text-gray-700 hover:bg-purple-100 p-2 rounded">
-          Liquidity Pool
+        <li className="text-[#6F7074] text-[11.25px] flex items-center  rounded-lg cursor-pointer transition-all duration-200">
+          <span className="w-5 h-5">
+            <img src={liquidityPool} alt="home" />
+          </span>
+          <span className="ml-1">Liquidity Pool</span>
         </li>
-        <li className="text-gray-700 hover:bg-purple-100 p-2 rounded">
-          Transaction Management
+        <li className="text-[#6F7074] text-[11.25px] flex items-center  rounded-lg cursor-pointer transition-all duration-200">
+          <span className="w-5 h-5">
+            <img src={transactionManagement} alt="home" />
+          </span>
+          <span className="ml-1">Transaction Management</span>
         </li>
-        <li className="text-gray-700 hover:bg-purple-100 p-2 rounded">
-          User Management
+        <li className="text-[#6F7074] text-[11.25px] flex items-center rounded-lg cursor-pointer transition-all duration-200">
+          <span className="w-5 h-5">
+            <img src={userManagement} alt="home" />
+          </span>
+          <span className="ml-1">User Management</span>
         </li>
-        <li className="text-gray-700 hover:bg-purple-100 p-2 rounded">
-          Payment Gateway
+        <li className="text-[#6F7074] text-[11.25px] flex items-center rounded-lg cursor-pointer transition-all duration-200">
+          <span className="w-5 h-5">
+            <img src={paymentGateway} alt="home" />
+          </span>
+          <span className="ml-1">Payment Gateway</span>
         </li>
-        <li className="text-gray-700 hover:bg-purple-100 p-2 rounded">
-          Staking Pool
+        <li className="text-[#6F7074] text-[11.25px] flex items-center  rounded-lg cursor-pointer transition-all duration-200">
+          <span className="w-5 h-5">
+            <img src={stakingPool} alt="home" />
+          </span>
+          <span className="ml-1">Staking Pool</span>
         </li>
-        <li className="text-gray-700 hover:bg-purple-100 p-2 rounded">
-          Profile
+        <li className="text-[#6F7074] text-[11.25px] flex items-center  rounded-lg cursor-pointer transition-all duration-200">
+          <span className="w-5 h-5">
+            <img src={profile} alt="home" />
+          </span>
+          <span className="ml-1">Profile</span>
         </li>
       </ul>
     </div>
